17edf2664772f03ab4aa43a567305f1a7f036b56
ettrue124 <98704701+ettrue124@users.noreply.github.com>
2025-09-12 11:15:37 -0500

    Use environment variables for secrets in SECRETS.js

--- SECRETS.js
+++ SECRETS.js
@@ -1,18 +1,9 @@
-//? It is advisable to use environment variables instead of directly putting secrets in repository file but I have skipped this part as it would become complicated for many.
-//? Alternatively you can download the repository instead or forking and upload it from your account and keep it private, in that way, your secrets will not be exposed to the public.
-
-const APP_KEY = "w7WIQULvRDe0CotckaBiCjJJk"; 
-const APP_SECRET = "WMxCHO1qrwl0Ozutd3Jh4HfB506RrrQOGnjFUANFKgiqmEc1kj";
-const ACCESS_TOKEN = "1632856285845626884-9CQAyMAqPaOQaVFCHXmVwMW1UQPXFt";
-const ACCESS_SECRET = "f93StVmOWWhnKphT0Q7PFA2VJTOOHNmvZiqbsOiJYBBER";
-const GEMINI_API_KEY = "AIzaSyALO7lGI0YkPyDcMYdHco5PVy2mUskws7U";
-
 const SECRETS = {
-  APP_KEY,
-  APP_SECRET,
-  ACCESS_TOKEN,
-  ACCESS_SECRET,
-  GEMINI_API_KEY,
+  APP_KEY: process.env.APP_KEY,
+  APP_SECRET: process.env.APP_SECRET,
+  ACCESS_TOKEN: process.env.ACCESS_TOKEN,
+  ACCESS_SECRET: process.env.ACCESS_SECRET,
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY,
 };
 
 module.exports = SECRETS;
